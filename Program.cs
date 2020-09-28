1a7041ed1899bef9c08cabec03f427d096a4750d
alberteje66 <alberteje66@gmail.com>
2020-09-28 10:29:20 -0400

    delegatepractice

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 namespace Geometry
 {
+    public delegate double Area(double a);
+    public delegate double Perimeter (double a);
     class Program
     {
         static void Main(string[] args)
