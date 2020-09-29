e6d73986fa249f64aa5185c838dc81d2fa538c60
alberteje66 <alberteje66@gmail.com>
2020-09-29 09:29:39 -0400

    More Code Clean Up. Delegates will be added today

--- PythagoreanTheorem.cs
+++ PythagoreanTheorem.cs
@@ -5,16 +5,17 @@ namespace Geometry
     public sealed class PythagoreanTheorem
     {
         double _a, _b, _c;
-        public TriSides _sides;
+        // public TriSides _sides;
 
          public PythagoreanTheorem(double a, double b, double c)
         {
             _a =a;
             _b = b;
             _c = c;
-            _sides.A = _a;
+            /* _sides.A = _a;
             _sides.B = _b;
-            _sides.C = _c;
+            _sides.C = _c; 
+            don't need these anymore, changed the RightTriangle part*/
         }
 
         public double CLength() {
