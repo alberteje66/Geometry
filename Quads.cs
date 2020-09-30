df4fc48d902c74091573ee24690b3c19a6f639b9
alberteje66 <71100872+alberteje66@users.noreply.github.com>
2020-09-29 22:30:31 -0400

    Changes for tomorro
    for the issue tomorrow

--- Quads.cs
+++ Quads.cs
@@ -1,56 +1,24 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Geometry {
-    class Quads : IShape {
-        private bool IsSquare; //differentiates between square and rectangles
-        private double _width;        
-        private double _length;
-        private double _height;
-        private bool _rightAngles;
-        private Shape _quads;
-        private PythagoreanTheorem pythagorean;
-
-        public Quads(double length, double width)
+    class Square : IQuadrilaterals {
+        public double GetPerimeter()
         {
-            _length = length;
-            _width = width;
-            pythagorean = new PythagoreanTheorem(_height, _length, _width);
-            _quads = new Shape(_height, _length, _width);
+            double perimeter = 0;
+            return perimeter;
         }
 
-        private bool RightAngle; //only squares and rectangles have right angles
-
-        public double GetPerimeter () {
-           double perimeter = 1;
-           if (!IsSquare)
-           {
-               perimeter = (2 * _length) + (2 * _width);
-           }
-           else
-           {
-               perimeter = _width * 4;
-           }
-
-           return perimeter;
-        }
-
-        public double GetArea () {
-            double area = 1;
-            if (IsSquare) {
-                area = Math.Pow (_width, 2);
-            } else if (!IsSquare) {
-                if (RightAngle)
-                {
-                    area = _length * _width;
-                }
-            } else {
-                //takes into account parallelograms
-                double height = pythagorean.ALength();
-                area = _width * _height;
-            }
-
+        public double GetArea()
+        {
+            double area = 0;
             return area;
         }
-    }
 
+        public double Line1 { get; set; }
+        public double Line2 { get; set; }
+        public double Line3 { get; set; }
+        public double Line4 { get; set; }
+    }
+    }
 }
